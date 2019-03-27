bc19c433be2af953b689310bf8d7eb777ff1c3c0
ReverendHeat <8883217+Reverendheat@users.noreply.github.com>
2019-03-27 15:02:25 -0400

    Fixed null type in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
+import { SIGN_IN, SIGN_OUT} from './types';
+
 //Two action creators for SignIn and SignOut
-export const signIn = () => {
+export const signIn = (userId) => {
     return {
-        type: 'SIGN_IN'
+        type: SIGN_IN,
+        payload: userId
     };
 };
 
 export const signOut = () => {
     return {
-        type: 'SIGN_OUT'
+        type: SIGN_OUT
     };
 };
---
+++ src/actions/types.js
@@ -0,0 +1,3 @@
+//To avoid typos in calling actions and reducers
+export const SIGN_IN = 'SIGN_IN';
+export const SIGN_OUT = 'SIGN_OUT';
